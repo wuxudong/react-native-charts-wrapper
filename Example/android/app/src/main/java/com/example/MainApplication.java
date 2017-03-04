5cec260fc8831afb024843a2a6c84ba446c216b9
wuxudong <xudong82113@gmail.com>
2017-03-04 20:18:45 +0800

    Update MainApplication.java
    remove duplicate import

--- Example/android/app/src/main/java/com/example/MainApplication.java
+++ Example/android/app/src/main/java/com/example/MainApplication.java
@@ -25,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MPAndroidChartPackage(),
             new MPAndroidChartPackage()
       );
     }
