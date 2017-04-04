308e0b56c5fa484b11deace44e2100f5777434dd
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2017-04-04 08:45:27 +0800

    add missing HorizontalBarChartManager

---
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/HorizontalBarChartManager.java
@@ -0,0 +1,27 @@
+package com.github.wuxudong.rncharts.charts;
+
+import android.view.View;
+
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.wuxudong.rncharts.data.BarDataExtract;
+import com.github.wuxudong.rncharts.data.DataExtract;
+import com.github.wuxudong.rncharts.listener.RNOnChartValueSelectedListener;
+
+public class HorizontalBarChartManager extends BarChartManager {
+
+    @Override
+    public String getName() {
+        return "RNHorizontalBarChart";
+    }
+
+    @Override
+    protected View createViewInstance(ThemedReactContext reactContext) {
+        HorizontalBarChart horizontalBarChart = new HorizontalBarChart(reactContext);
+        horizontalBarChart.setOnChartValueSelectedListener(new RNOnChartValueSelectedListener(horizontalBarChart));
+        return horizontalBarChart;
+    }
+}
