90828dfe7cf646edd7bcd972b846281f57ee52c1
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2017-04-23 21:09:49 +0800

    fix pie entryLable style

--- ios/ReactNativeCharts/pie/RNPieChartView.swift
+++ ios/ReactNativeCharts/pie/RNPieChartView.swift
@@ -89,11 +89,10 @@ class RNPieChartView: RNChartViewBase {
     }
   
     override func didSetProps(_ changedProps: [String]!) {
-        super.didSetProps(changedProps);
         let pieChartDataSet = chart.data?.dataSets[0] as? PieChartDataSet
     
         pieChartDataSet?.entryLabelColor = chart.entryLabelColor
         pieChartDataSet?.entryLabelFont = chart.entryLabelFont
-  }  
+  }
   
 }
