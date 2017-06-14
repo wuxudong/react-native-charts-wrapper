f982677af18a64975a2f8b279e5b3e5d845272e2
Chenyu Li <idealllee@gmail.com>
2017-06-14 15:59:50 -0700

    port viewportOffsets

--- android/src/main/java/com/github/wuxudong/rncharts/charts/BarLineChartBaseManager.java
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/BarLineChartBaseManager.java
@@ -119,4 +119,23 @@ public abstract class BarLineChartBaseManager<T extends BarLineChartBase, U exte
             );
         }
     }
+
+    @ReactProp(name = "viewPortOffsets")
+    public void setViewPortOffsets(Chart chart, ReadableMap propMap) {
+        Integer left = 0, top = 0, right = 0, bottom = 0;
+
+        if (BridgeUtils.validate(propMap, ReadableType.Number, "left")) {
+            left = propMap.getInt("left");
+        }
+        if (BridgeUtils.validate(propMap, ReadableType.Number, "top")) {
+            top = propMap.getInt("top");
+        }
+        if (BridgeUtils.validate(propMap, ReadableType.Number, "right")) {
+            right = propMap.getInt("right");
+        }
+        if (BridgeUtils.validate(propMap, ReadableType.Number, "bottom")) {
+            bottom = propMap.getInt("bottom");
+        }
+        ((BarLineChartBase) chart).setViewPortOffsets(left, top, right, bottom);
+    }
 }
