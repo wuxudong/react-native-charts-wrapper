cc501131853f4b6a5dee307dab4561052aba009b
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2017-09-05 11:25:43 -0300

    Added missing DateFormatter class.

--- android/src/main/java/com/github/wuxudong/rncharts/utils/ChartDataSetConfigUtils.java
+++ android/src/main/java/com/github/wuxudong/rncharts/utils/ChartDataSetConfigUtils.java
@@ -11,6 +11,7 @@ import com.github.mikephil.charting.data.LineScatterCandleRadarDataSet;
 import com.github.mikephil.charting.formatter.LargeValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.wuxudong.rncharts.charts.CustomFormatter;
+import com.github.wuxudong.rncharts.charts.DateFormatter;
 
 /**
  * https://github.com/PhilJay/MPAndroidChart/wiki/The-DataSet-class
