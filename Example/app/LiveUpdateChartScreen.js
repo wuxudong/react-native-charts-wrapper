afc4700ad95225e2a337e4def956cd39e8c7730d
xudong82113 <xudong82113@gmail.com>
2017-11-20 15:14:43 +0800

    fix LiveUpdateChart Example, add default data. MpAndroidChart lineChart will crash when data entries is empty

--- Example/app/LiveUpdateChartScreen.js
+++ Example/app/LiveUpdateChartScreen.js
@@ -20,7 +20,7 @@ export default class LiveUpdating extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      values: [],
+      values: [0],
       colorIndex: 0
     }
   }
@@ -53,8 +53,11 @@ export default class LiveUpdating extends Component {
 
   componentDidMount() {
     this.interval = setInterval(() => {
-      if (this.state.values.length >= 20) {
-        this.setState({values: [], colorIndex: 0});
+      if (this.state.values.length >= 60) {
+        // https://github.com/PhilJay/MPAndroidChart/issues/2450
+        // MpAndroidChart 3.0.2 will crash when data entry list is empty.
+
+        this.setState({values: [0], colorIndex: 0});
       } else {
         this.setState({
           values: this.state.values.concat([Math.floor((Math.random() * 100) + 1)]),
