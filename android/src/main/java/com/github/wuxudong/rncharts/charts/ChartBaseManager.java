ccdc5c9b6109d420595cdb2dac2b7ae7d94b5fbe
David Liu <dl@voxel.com>
2018-03-08 14:11:03 -0800

    Basic fontFamily axis support for android

--- android/src/main/java/com/github/wuxudong/rncharts/charts/ChartBaseManager.java
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/ChartBaseManager.java
@@ -1,14 +1,15 @@
 package com.github.wuxudong.rncharts.charts;
 
 import android.content.res.ColorStateList;
+import android.graphics.Typeface;
 import android.os.Build;
-import android.view.View;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.views.text.ReactFontManager;
 import com.github.mikephil.charting.animation.Easing.EasingOption;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -26,16 +27,12 @@ import com.github.mikephil.charting.formatter.LargeValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.wuxudong.rncharts.data.DataExtract;
-import com.github.wuxudong.rncharts.listener.RNOnChartValueSelectedListener;
 import com.github.wuxudong.rncharts.markers.RNRectangleMarkerView;
 import com.github.wuxudong.rncharts.utils.BridgeUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.annotation.Nullable;
 
 public abstract class ChartBaseManager<T extends Chart, U extends Entry> extends SimpleViewManager<T> {
 
@@ -287,6 +284,30 @@ public abstract class ChartBaseManager<T extends Chart, U extends Entry> extends
         if (BridgeUtils.validate(propMap, ReadableType.Number, "textSize")) {
             axis.setTextSize((float) propMap.getDouble("textSize"));
         }
+        if (BridgeUtils.validate(propMap, ReadableType.String, "fontFamily")) {
+            String fontFamily = propMap.getString("fontFamily");
+            boolean italic = false;
+            boolean bold = false;
+            int style = Typeface.NORMAL;
+            if (BridgeUtils.validate(propMap, ReadableType.String, "fontStyle")) {
+                italic = "italic".equals(propMap.getString("fontStyle"));
+            }
+            if (BridgeUtils.validate(propMap, ReadableType.String, "fontWeight")) {
+                bold = "bold".equals(propMap.getString("fontWeight"));
+            }
+
+            if (italic && bold) {
+                style = Typeface.BOLD_ITALIC;
+            } else if (italic) {
+                style = Typeface.ITALIC;
+            } else if (bold) {
+                style = Typeface.BOLD;
+            }
+
+            axis.setTypeface(ReactFontManager.getInstance().getTypeface(fontFamily,
+                    style,
+                    chart.getContext().getAssets()));
+        }
         if (BridgeUtils.validate(propMap, ReadableType.Number, "gridColor")) {
             axis.setGridColor(propMap.getInt("gridColor"));
         }
@@ -340,7 +361,7 @@ public abstract class ChartBaseManager<T extends Chart, U extends Entry> extends
                     if (BridgeUtils.validate(limitLineMap, ReadableType.Number, "lineWidth")) {
                         limitLine.setLineWidth((float) limitLineMap.getDouble("lineWidth"));
                     }
-                    
+
                     if (BridgeUtils.validate(limitLineMap, ReadableType.Number, "valueTextColor")) {
                         limitLine.setTextColor(limitLineMap.getInt("valueTextColor"));
                     }
@@ -352,11 +373,11 @@ public abstract class ChartBaseManager<T extends Chart, U extends Entry> extends
                     }
                     if (BridgeUtils.validate(limitLineMap, ReadableType.Number, "lineDashPhase")
                             && BridgeUtils.validate(limitLineMap, ReadableType.Array, "lineDashLengths")) {
-                        if (limitLineMap.getArray("lineDashLengths").size()>1) {
+                        if (limitLineMap.getArray("lineDashLengths").size() > 1) {
                             float lineDashPhase = (float) limitLineMap.getDouble("lineDashPhase");
-                            float lineLength =  limitLineMap.getArray("lineDashLengths").getInt(0);
+                            float lineLength = limitLineMap.getArray("lineDashLengths").getInt(0);
                             float spaceLength = limitLineMap.getArray("lineDashLengths").getInt(1);
-                            limitLine.enableDashedLine(lineLength,spaceLength, lineDashPhase);
+                            limitLine.enableDashedLine(lineLength, spaceLength, lineDashPhase);
                         }
                     }
 
