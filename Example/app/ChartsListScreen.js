2449a993b6a0b8d99169588d5475397dc4ef9937
Antoine Hanriat <ahanriat@gmail.com>
2018-04-21 08:27:44 +0200

    Add fillGradient (#260)

--- Example/app/ChartsListScreen.js
+++ Example/app/ChartsListScreen.js
@@ -19,6 +19,7 @@ import BubbleChartScreen from './BubbleChartScreen';
 import CandleStickChartScreen from './CandleStickChartScreen';
 import CombinedChartScreen from './CombinedChartScreen';
 import LineChartScreen from './LineChartScreen';
+import LineChartGradientScreen from './LineChartGradientScreen';
 import TimeSeriesLineChartScreen from './TimeSeriesLineChartScreen';
 import PieChartScreen from './PieChartScreen';
 import RadarChartScreen from './RadarChartScreen';
@@ -75,6 +76,11 @@ var ExampleRoutes = {
     name: 'LineChart',
     screen: LineChartScreen,
     description: 'Displays a LineChart',
+  },
+  LineChartGradientScreen: {
+    name: 'LineChartGradient',
+    screen: LineChartGradientScreen,
+    description: 'Displays a LineChart with Gradient',
   },
   RadarChartScreen: {
     name: 'RadarChart',
