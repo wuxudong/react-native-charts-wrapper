9711489caaf5b92ade00a2d93752d62a194496d8
Yaroslav Stasiuk <yastasyuk@gmail.com>
2019-05-03 10:00:54 +0300

    [iOS]: Large value works incorrecty for small ranges and negative numbers (#521)
    * Fixed LargeValueFormatter to support negative numbers and updated label format to 2 digits after decimal point
    
    * Fixed LargeValueFormatter: Removed incorrect "+" for positive numbers

--- ios/ReactNativeCharts/formatters/LargeValueFormatter.swift
+++ ios/ReactNativeCharts/formatters/LargeValueFormatter.swift
@@ -9,53 +9,58 @@ import Charts
 open class LargeValueFormatter: NSObject, IValueFormatter, IAxisValueFormatter
 {
     fileprivate static let MAX_LENGTH = 5
-    
+
     /// Suffix to be appended after the values.
     ///
     /// **default**: suffix: ["", "k", "m", "b", "t"]
     open var suffix = ["", "k", "m", "b", "t"]
-    
+
     /// An appendix text to be added at the end of the formatted value.
     open var appendix: String?
-    
+
     public override init()
     {
-        
+
     }
-    
+
     public init(appendix: String?)
     {
         self.appendix = appendix
     }
-    
+
     fileprivate func format(_ value: Double) -> String
     {
-        var sig = value
+        var sig = abs(value)
+        let sign = value < 0 ? "-" : ""
         var length = 0
         let maxLength = suffix.count - 1
-        
+
         while sig >= 1000.0 && length < maxLength
         {
             sig /= 1000.0
             length += 1
         }
-        
-        var r = String(format: "%2.f", sig) + suffix[length]
-        
+
+        let valueFormatter = NumberFormatter()
+        valueFormatter.maximumFractionDigits = 2
+        valueFormatter.minimumFractionDigits = 0
+
+        var r = sign + (valueFormatter.string(from: NSNumber(value: sig)) ?? "") + suffix[length]
+
         if appendix != nil
         {
             r += appendix!
         }
-        
+
         return r
     }
-    
+
     open func stringForValue(
         _ value: Double, axis: AxisBase?) -> String
     {
         return format(value)
     }
-    
+
     open func stringForValue(
         _ value: Double,
         entry: ChartDataEntry,
