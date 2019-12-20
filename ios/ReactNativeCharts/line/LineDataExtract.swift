dd4cfa614fa0701f21de6158d771a1dd4c407a6b
CHOIMINSEOK <cms3718@gmail.com>
2019-12-20 18:22:34 +0900

    iOS: Implement Icon Resize

--- ios/ReactNativeCharts/line/LineDataExtract.swift
+++ ios/ReactNativeCharts/line/LineDataExtract.swift
@@ -6,6 +6,7 @@ import Foundation
 
 import SwiftyJSON
 import Charts
+import UIKit
 
 class LineDataExtract : DataExtract {
     override func createData() -> ChartData {
@@ -111,7 +112,17 @@ class LineDataExtract : DataExtract {
                     let bundle = icon["bundle"];
                     
                     let uiImage = RCTConvert.uiImage(bundle.dictionaryObject);
-                    entry = ChartDataEntry(x: x, y: dict["y"].doubleValue, icon: uiImage);
+                    let width = CGFloat(icon["width"].numberValue);
+                    let height = CGFloat(icon["height"].numberValue);
+                    
+                    if let image = uiImage {
+                        let realIconImage = resizeImage(image: image, width: width, height: height);
+                        entry = ChartDataEntry(x: x, y: dict["y"].doubleValue, icon: realIconImage);
+                    } else {
+                        entry = ChartDataEntry(x: x, y: dict["y"].doubleValue, icon: uiImage);
+                    } 
+                    
+                    
                 } else {
                     entry = ChartDataEntry(x: x, y: dict["y"].doubleValue, data: dict as AnyObject?);
                 }
@@ -126,5 +137,32 @@ class LineDataExtract : DataExtract {
         }
         
         return entry;
+    }
+
+    func resizeImage(image: UIImage, width: CGFloat, height: CGFloat) -> UIImage {
+      let targetSize = CGSize(width: width, height: height)
+      let size = image.size
+
+      let widthRatio  = targetSize.width  / size.width
+      let heightRatio = targetSize.height / size.height
+
+      // Figure out what our orientation is, and use that to form the rectangle
+      var newSize: CGSize
+      if(widthRatio > heightRatio) {
+          newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+      } else {
+          newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+      }
+
+      // This is the rect that we've calculated out and this is what is actually used below
+      let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+      // Actually do the resizing to the rect using the ImageContext stuff
+      UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+      image.draw(in: rect)
+      let newImage = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+
+      return newImage!
     }
 }
