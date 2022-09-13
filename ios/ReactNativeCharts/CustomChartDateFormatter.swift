dfefd0dd05a5b03107bc7e1e7edaf792c806e300
Petter Ljungqvist <petter.ljungqvist@houston-inc.com>
2022-09-13 15:19:49 +0300

    Charts 4.1.0 upgrade

--- ios/ReactNativeCharts/CustomChartDateFormatter.swift
+++ ios/ReactNativeCharts/CustomChartDateFormatter.swift
@@ -8,37 +8,37 @@
 import Foundation
 import Charts
 
-open class CustomChartDateFormatter: NSObject, IValueFormatter, IAxisValueFormatter {
-    
+open class CustomChartDateFormatter: NSObject, ValueFormatter, AxisValueFormatter {
+
     open var dateFormatter = DateFormatter();
-    
+
     open var since = 0.0
-    
+
     open var timeUnit : String?
-    
+
     public override init() {
-        
+
     }
-    
+
     public init(pattern: String?, since: Double, timeUnit: String?, locale: String?) {
         self.dateFormatter.locale = Locale(identifier: locale ?? Locale.current.languageCode ?? "en_US");
         self.dateFormatter.dateFormat = pattern;
         self.since = since
         self.timeUnit = timeUnit
     }
-    
+
     open func stringForValue(_ value: Double, axis: AxisBase?) -> String {
         return format(value)
     }
-    
+
     open func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
         return format(value)
     }
-    
+
     fileprivate func format(_ value: Double) -> String
     {
         var span = 0.0
-        
+
         // TODO: any better enum other than String equals to java.util.TimeUnit
         switch timeUnit {
         case "MILLISECONDS":
@@ -54,11 +54,11 @@ open class CustomChartDateFormatter: NSObject, IValueFormatter, IAxisValueFormat
         default:
             span = value / 1000.0
         }
-        
+
         let timeIntervalSince1970 = self.since / 1000.0 +  span
-        
+
         let date = Date(timeIntervalSince1970: timeIntervalSince1970);
         return self.dateFormatter.string(from: date);
     }
-    
+
 }
